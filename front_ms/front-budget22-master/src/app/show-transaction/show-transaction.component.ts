17372c414ce122a21eaa746f47acdb95b899a4bf
Mohamed Iheb Lahzami <92752587+iheblahzami@users.noreply.github.com>
2024-06-10 11:03:40 +0100

    18

---
+++ front_ms/front-budget22-master/src/app/show-transaction/show-transaction.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Transaction } from '../models/Transaction';
+import { TransactionService } from '../services/TransactionServiceImpl';
+
+@Component({
+  selector: 'app-edit-transaction',
+  templateUrl: './show-transaction.component.html',
+  styleUrls: ['./show-transaction.component.css']
+})
+export class ShowTransactionComponent implements OnInit {
+  transactionForm!: FormGroup;
+  transactionId!: number;
+  transaction!: Transaction;
+  transactions: Transaction[] = [];
+  constructor(
+    private formBuilder: FormBuilder,
+    private transactionService: TransactionService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+  ;
+
+    this.getAllTransactions();
+  }
+
+
+
+  onSubmit(): void {
+  }
+
+
+
+  getAllTransactions(): void {
+    this.transactionService.getAllTransactions().subscribe(
+      (transactions: Transaction[]) => {
+        this.transactions = transactions;
+      },
+      error => {
+        console.error('Error fetching transactions:', error);
+      }
+    );
+  }
+}
---
+++ front_ms/front-budget22-master/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  config: {
+    urlLogin: 'http://127.0.0.1:8005'
+    // urlLogin: 'http://51.8.241.244:8005'
+  },
+  // budgetUrl: 'http://51.8.52.144:9000/budget',
+  // incomeUrl: 'http://51.8.52.144:9000/income',
+  // transactionApiUrl: 'http://57.151.45.201:9001/transaction',
+  // userApiUrl: 'http://57.151.45.210:9005',
+  // apiUrl: 'http://budgetapp.us.to'
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
